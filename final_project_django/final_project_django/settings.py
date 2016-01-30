35ed24c4357fc43ea9e3505f6fb5ec0de53f6b51
ruteq <rufatf@gmail.com>
2016-01-30 11:04:10 +0300

    htp  final project

--- final_project_django/final_project_django/settings.py
+++ final_project_django/final_project_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fp_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'final_project_django.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'xo_db',
+       'USER': 'root',
+       'PASSWORD': 'root',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
 }
 
 
