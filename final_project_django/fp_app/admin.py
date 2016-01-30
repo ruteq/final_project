d6747af399663821e7e6926628f4b7b8c30c3aa3
ruteq <rufatf@gmail.com>
2016-01-30 11:55:17 +0300

    htp  final project

--- final_project_django/fp_app/admin.py
+++ final_project_django/fp_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Players, PlayerAchievements, PlayerSessions, PlayerStats, LogGameEvents
+
 
 # Register your models here.
+
+admin.site.register([Players, PlayerAchievements, PlayerSessions, PlayerStats, LogGameEvents])
